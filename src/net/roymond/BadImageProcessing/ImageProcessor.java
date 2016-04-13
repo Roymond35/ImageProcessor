00481f7b5cf0e51d2472578bc3f0a51208fc9f27
Roy Gero <roy.gero.ii@gmail.com>
2016-04-13 13:55:58 -0400

    Merging the GUI Prep Refactoring branch into the master stream and tidying up everything.

---
+++ src/net/roymond/BadImageProcessing/ImageProcessor.java
@@ -0,0 +1,84 @@
+package net.roymond.BadImageProcessing;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by gero on 4/13/2016.
+ */
+public class ImageProcessor {
+
+    private int delta;
+    private String inputFilePath;
+    private BufferedImage sourceImage;
+    private String exportDir;
+    private Graphics2D graphic;
+
+    public void exportFile(){
+        long currentTime = System.currentTimeMillis();
+        String fileName = "shitty_" + String.valueOf(currentTime) + ".png";
+        File outputFile = new File(exportDir + "\\" + fileName);
+
+        graphic.drawImage(sourceImage, null, 0, 0);
+        try {
+            ImageIO.write(sourceImage, "png", outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean loadImage(){
+        sourceImage = null;
+        try {
+            sourceImage = ImageIO.read(new File(inputFilePath));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean processImage(){
+        if(loadImage()) {
+            int imgWidth = sourceImage.getWidth();
+            int imgHeight = sourceImage.getHeight();
+            int changeX = delta / 2;
+            int changeY = delta / 2;
+            int radius = delta / 2;
+            int pixelColor;
+            int posX = 0;
+            int posY = 0;
+            graphic = sourceImage.createGraphics();
+            while (posY < imgHeight) {
+                while (posX < imgWidth) {
+                    pixelColor = sourceImage.getRGB(posX, posY);
+                    graphic.setColor(new Color(pixelColor));
+                    graphic.fillOval(posX, posY, radius, radius);
+                    posX = posX + changeX;
+                }
+                posY = posY + changeY;
+                posX = 0;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public ImageProcessor(String inputFilePath, int delta, String exportDirectory){
+        /*This is the constructor for the Image Processor class.
+
+        @param inputFilePath - The path to the input image
+        @param delta - This variable is used to generate the level of processing
+        @param exportDirectory - This variable is where the new image will be placed.
+         */
+        this.inputFilePath = inputFilePath;
+        this.delta = delta;
+        this.exportDir = exportDirectory;
+    }
+
+}
--- src/net/roymond/BadImageProcessing/Main.java
+++ src/net/roymond/BadImageProcessing/Main.java
@@ -1,78 +1,17 @@
 package net.roymond.BadImageProcessing;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 public class Main{
-
-    static int delta = 20;
-    static String dirOfFile;
-
-    public static void exportFile(Graphics2D graphic, BufferedImage image){
-        long currentTime = System.currentTimeMillis();
-        String fileName = "shitty_" + String.valueOf(currentTime) + ".png";
-        File outputFile = new File(fileName);
-        graphic.drawImage(image, null, 0, 0);
-        try {
-            ImageIO.write(image, "png", outputFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void processImage(BufferedImage img){
-        int imgWidth = img.getWidth();
-        int imgHeight = img.getHeight();
-        Graphics2D graphic = img.createGraphics();
-        int changeX = delta/2;
-        int changeY = delta/2;
-        int radius = delta/2;
-        int pixelColor;
-        int posX = 0;
-        int posY = 0;
-
-        while(posY < imgHeight){
-            while(posX < imgWidth){
-                pixelColor = img.getRGB(posX,posY);
-                graphic.setColor(new Color(pixelColor));
-                graphic.fillOval(posX,posY,radius,radius);
-                posX = posX + changeX;
-            }
-            posY = posY + changeY;
-            posX = 0;
-        }
-        exportFile(graphic,img);
-    }
-
-
 
     public static void main(String[] args) {
 
-        //Getting the directory
-        try {
-            dirOfFile = args[0];
-            delta = Integer.parseInt(args[1]);
-        }
-        catch (IndexOutOfBoundsException e){
-            System.out.println("FAILED!");
-            dirOfFile = "";
+        String inputFile = "C:\\ImageProcessor\\File_000.jpeg";
+        int delta = 20;
+        String exportDir = "C:\\ImageProcessor\\";
 
-        }
-
-        //Open an image
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File(dirOfFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (img != null){
-            processImage(img);
-        }
+        ImageProcessor imageProcessor = new ImageProcessor(inputFile, delta, exportDir);
+        imageProcessor.loadImage();
+        imageProcessor.processImage();
+        imageProcessor.exportFile();
 
     }
 }
